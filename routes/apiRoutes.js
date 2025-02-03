a8ded1b363797d60fb9ae3c324919e23cde5b1b0
Antonio Cabrero <49397161+Antonio-Cabrero@users.noreply.github.com>
2025-02-03 10:12:22 +0100

    get users done

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 // GET
-router.get("/users", (req, res) => {
+router.get("/users", async (req, res) => {
 	// get users as array
-	res.json({ greeting: "hello API" });
+	const users = await User.find();
+	if (!users) {
+		return res.status(404).json({ error: "No users found" });
+	}
+	res.json(users);
 });
 
 router.get("/users/:_id", (req, res) => {
