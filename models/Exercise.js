0015e5bf56ce2b13ea968616824b91d3b555f29f
Antonio Cabrero <49397161+Antonio-Cabrero@users.noreply.github.com>
2025-02-07 08:19:01 +0100

    changes to models

--- models/Exercise.js
+++ models/Exercise.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const exerciseSchema = new Schema({
 	description: { type: String, required: true },
 	duration: { type: Number, required: true },
-	date: { type: String, required: false },
+	date: { type: Date, required: false },
 });
 
 const Exercise = mongoose.model("Exercise", exerciseSchema);
--- models/User.js
+++ models/User.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
 	username: { type: String, required: true },
-	_id: { type: Schema.Types.ObjectId, auto: true },
+	_id: { type: Schema.ObjectId, auto: true },
 });
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = { User, userSchema };
+module.exports = User;
